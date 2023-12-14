706666c5d8b196f8b3649a0db94730aca4efbef0
Vadim <fw.mbagg@gmail.com>
2023-12-14 08:01:22 +0300

    fix header

--- src/components/HeaderControls.tsx
+++ src/components/HeaderControls.tsx
@@ -4,13 +4,13 @@ export const HeaderControls = () => {
     return (
         <header>
             <nav className="border-b-2 border-stone-600 h-16 control-header flex justify-center">
-                <p className="flex absolute text-3xl items-center start-0 p-3 control-header">QMS</p>
+                <p className="flex absolute text-4xl items-center start-0 p-3 control-header font-bold">QMS</p>
                 <div className="flex items-center relative gap-11 justify-center text-xl text-gray-500 font-medium">
-                    <div>Features</div>
-                    <div>Customers</div>
-                    <div>Buisness</div>
-                    <div>Pricing</div>
-                    <div>Company</div>
+                    <div className="hover:text-stone-900 cursor-pointer">Features</div>
+                    <div className="hover:text-stone-900 cursor-pointer">Customers</div>
+                    <div className="hover:text-stone-900 cursor-pointer">Buisness</div>
+                    <div className="hover:text-stone-900 cursor-pointer">Pricing</div>
+                    <div className="hover:text-stone-900 cursor-pointer">Company</div>
                 </div>
             </nav>
         </header>
