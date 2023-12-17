e50f9ffa46083b29806a0cfeb51447805dfaeba2
Vadim <fw.mbagg@gmail.com>
2023-12-17 22:49:30 +0300

    fix gap on stub

--- src/pages/features/Features.tsx
+++ src/pages/features/Features.tsx
@@ -3,7 +3,7 @@ import "../../index.css"
 
 export const FeaturesPage = () => {
     return (
-        <div className="flex justify-center gap-5 mt-96">
+        <div className="flex justify-center gap-3 mt-96">
             <p className="text-2xl text-qms font-bold">We are working on this page </p>
             <RefreshCw className="animate-spin" />
             <button className="bg-stone-800 rounded-3xl absolute px-10 py-3 mt-12 text-white text-qms font-bold">
