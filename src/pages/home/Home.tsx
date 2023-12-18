17defefd1bb81c6773bb35ea1fbd16d7fcfdf027
Vadim <fw.mbagg@gmail.com>
2023-12-18 08:00:48 +0300

    fix home page text size

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,7 +7,7 @@ export const HomePage = () => {
         <>
             <HeaderControls />
             <div className="mx-24 mt-10">
-                <p className="font-bold text-xl mb-10 mt-20 qms">Quality Management System</p>
+                <p className="font-bold text-2xl mb-10 mt-20 qms">Quality Management System</p>
                 <p className="text-8xl featured font-bold">Full-featured quality system,<br />
                     business-oriented to achieve <br /> results aligned with QA engineers
                 </p>
@@ -16,11 +16,11 @@ export const HomePage = () => {
                 <div className="flex justify-center gap-14 text-qms mt-16">
                     <div className="mr-14">
                         <p className="text-3xl font-bold mb-3">Overview</p>
-                        <p className="text-xl mt-5">We have integrated the experience of many quality engineers and made a tool that indicates the exact quality value of your project based on the development management system and the coverage of their checks.</p>
+                        <p className="text-2xl mt-5">We have integrated the experience of many quality engineers and made a tool that indicates the exact quality value of your project based on the development management system and the coverage of their checks.</p>
                     </div>
                     <div>
-                        <p className="text-2xl font-bold mb-3">Approach</p>
-                        <p className="text-xl mt-5">Based on our practical experience, we have created a system that allows you to obtain an assessment of the quality of coverage by inspection of any project of any size. The system is suitable form many areas: Internet technology, device design, manufacturing, aircraft manufacturing, etc.</p>
+                        <p className="text-3xl font-bold mb-3">Approach</p>
+                        <p className="text-2xl mt-5">Based on our practical experience, we have created a system that allows you to obtain an assessment of the quality of coverage by inspection of any project of any size. The system is suitable form many areas: Internet technology, device design, manufacturing, aircraft manufacturing, etc.</p>
                     </div>
                 </div>
 
@@ -31,7 +31,7 @@ export const HomePage = () => {
                     <div>
                         <p className="text-8xl featured font-bold"> Quality is an <br /> indicator <br/> measured in <br />numbers</p>
                         <p className="mt-5 text-3xl text-stone-500">We collect and analyze the <br /> percentage of coverage by test <br /> checks for all the functionality you <br /> have implemented</p>
-                        <button className="btn-qms py-3 px-10 mt-5 mb-10"><a href="/free-trial">Get Started</a></button>
+                        <button className="btn-qms py-3 px-10 mt-10 mb-10"><a href="/free-trial">Get Started</a></button>
                     </div>
                 </div>
             </div>
